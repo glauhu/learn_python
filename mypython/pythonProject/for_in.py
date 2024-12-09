65f678d8bdb710cdc914734a77b1467352e5d2c3
山胡椒 <40205602+glauhu@users.noreply.github.com>
2024-12-09 22:42:14 +0800

    Add files via upload

---
+++ mypython/pythonProject/for_in.py
@@ -0,0 +1,5 @@
+#循环语句 for 的用法
+char_list=["a","b","c","d","e"]
+for char_1 in char_list:
+    print(char_1)
+
---
+++ mypython/pythonProject/hello_world2.py
@@ -0,0 +1,8 @@
+message = "Hello World!"
+print(message)
+
+#首字母大写
+name = 'love is good'
+print(name.title())
+
+import this
