8bf24b0d667723169d114b6c46ba2009e41c0cda
山胡椒 <40205602+glauhu@users.noreply.github.com>
2024-12-19 16:23:18 +0800

    Add files via upload

---
+++ mypython/pythonProject/dict01.py
@@ -0,0 +1,12 @@
+
+alien_0 = {"color":'green','points':5}
+print(alien_0)
+print('color is ',alien_0["color"])
+
+alien_0['x_position']=10
+alien_0['y_position']=10
+print (alien_0)
+
+alien_0['color'] ='red'
+print(alien_0)
+
